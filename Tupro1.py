81f2941958a5a8fef52a6a0214e70b2b47b25f2e
Chika <58129889+itsrafiif@users.noreply.github.com>
2021-03-23 22:24:21 +0700

    updated

--- Tupro1.py
+++ Tupro1.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-def h(x, y):                   #rumus nilai heuristic
+def heuristic(x, y):                   #rumus nilai heuristic
     return (math.cos(x*x) * math.sin(y*y)) + (x + y)
 
 rb_x = -1       # batas bawah x
@@ -10,76 +10,77 @@ rb_y = -1       # batas bawah y
 ra_y = 1        # batas atas y
 
 class Kromosom:                     #Class untuk menampung kromosom
-    def __init__(self, bit = None):
-        if bit == None:
-            self.bit = random.choices([0, 1], k=8)
+    def __init__(self, biner = None):
+        if biner == None:
+            self.biner = random.choices([0, 1], k=10)
         else:
-            self.bit = bit
-        self.x1 = self.rekombinasi(ra_x, rb_x, self.bit[:4])
-        self.x2 = self.rekombinasi(ra_y, rb_y, self.bit[4:])
+            self.biner = biner
+        self.x = self.Representasi(ra_x, rb_x, self.biner[:5])
+        self.y = self.Representasi(ra_y, rb_y, self.biner[5:])
 
-    def __repr__(self):
-        return '{} ({}, {}) - {}'.format(self.bit, self.x1, self.x2, h(self.x1, self.x2))
+    def __repr__(self):         #printable representation dari class kromosom
+        return '{} ({}, {}) - {}'.format(self.biner, self.x, self.y, heuristic(self.x, self.y))
 
-    def rekombinasi(self, ra, rb, g):
+    def Representasi(self, ra, rb, g):          #fungsi representasi biner
         tp = [2**-i for i in range(1, len(g) + 1)]
-        return rb + ((ra - rb) / sum(tp) * sum([g[i] * tp[i] for i in range(len(g))]))
+        return rb + ((ra - rb) / sum(tp) * sum([g[i] * tp[i] for i in range(len(g))]))  #rumus representasi biner
 
-def f(x1, x2):          #fungsi fitness: maksimum f = h
-    hasil = h(x1, x2)
-    return hasil
+def fitness(x, y):          #fungsi fitness: maksimum f = h
+    fitness_func = heuristic(x, y)
+    return fitness_func
 
 def exist(l, c):        #check jika kromosom ada di populasi dan digunakan juga
     found = False       #untuk seleksi orang tua
     for i in l:
-        if i.bit == c.bit:
+        if i.biner == c.biner:
             found = True
             break
     return found
 
-def seleksi_orangtua(k):    # fungsi seleksi orangtua mereturn orangtua dari
+def seleksi_orangtua(k):    # fungsi seleksi orangtua mereturn orangtua dari Roulette wheele
     orangtua = []           # populasi
-    arr_fitness = list(map(lambda c: f(c.x1, c.x2), populasi))
-    arr_weight = [arr_fitness[i] / sum(arr_fitness) for i in range(len(populasi))]
+    arr_fitness = list(map(lambda c: fitness(c.x, c.y), populasi))  # memakai fungsi lambda sebagai anonymous function
+    arr_weight = [arr_fitness[i] / sum(arr_fitness) for i in range(len(populasi))]  
     while len(orangtua) != k:
-        kandidat = random.choices(populasi, weights=arr_weight)[0]
-        if not exist(orangtua, kandidat):
+        kandidat = random.choices(populasi, weights=arr_weight)[0]  # parameter weight akan memberi berat kemungkinan pada setiap nilai
+        if not exist(orangtua, kandidat):                           # sehingga setiap item untuk dipilih ditentukan oleh bobot relatifnya.
             orangtua.append(kandidat)
     return orangtua
 
 
 def Crossover(ortu1, ortu2):        # fungsi untuk crossover menghasilkan anak
-    posisi = random.randint(1, len(ortu1.bit) - 2)
+    posisi = random.randint(1, len(ortu1.biner) - 2)
 
-    bit_anak1 = ortu1.bit[:posisi] + ortu2.bit[posisi:]
-    bit_anak2 = ortu2.bit[:posisi] + ortu1.bit[posisi:]
+    biner_anak1 = ortu1.biner[:posisi] + ortu2.biner[posisi:]
+    biner_anak2 = ortu2.biner[:posisi] + ortu1.biner[posisi:]
 
-
-    rng_mutasi = random.uniform(0, 100)
-    if rng_mutasi > (100 - 0.5): #0.5% mutasi
-        posisi_mutasi = random.randint(0, len(bit_anak1) - 1)
-        if bit_anak1[posisi_mutasi] == 1:
-            bit_anak1[posisi_mutasi] = 0
+    #Mutasi anak1
+    prob_mutasi = random.uniform(0, 100)    # pick angka random dari 0 sampai 100
+    if prob_mutasi > (100 - 0.5): #0.5% mutasi
+        posisi_mutasi = random.randint(0, len(biner_anak1) - 1)
+        if biner_anak1[posisi_mutasi] == 1:
+            biner_anak1[posisi_mutasi] = 0
         else:
-            bit_anak1[posisi_mutasi] = 1
+            biner_anak1[posisi_mutasi] = 1
 
-    rng_mutasi = random.uniform(0, 100)
-    if rng_mutasi > (100 - 0.5): #0.5% mutasi
-        posisi_mutasi = random.randint(0, len(bit_anak2) - 1)
-        if bit_anak2[posisi_mutasi] == 1:
-            bit_anak2[posisi_mutasi] = 0
+    #mutasi anak2
+    prob_mutasi = random.uniform(0, 100)    # pick angka random dari 0 sampai 100
+    if prob_mutasi > (100 - 0.5): #0.5% mutasi
+        posisi_mutasi = random.randint(0, len(biner_anak2) - 1)
+        if biner_anak2[posisi_mutasi] == 1:
+            biner_anak2[posisi_mutasi] = 0
         else:
-            bit_anak2[posisi_mutasi] = 1
+            biner_anak2[posisi_mutasi] = 1
 
 
-    populasi.append(Kromosom(bit_anak1))    # memasukan hasil crossover dan mutasi
-    populasi.append(Kromosom(bit_anak2))    # ke populasi
+    populasi.append(Kromosom(biner_anak1))    # memasukan hasil crossover dan mutasi
+    populasi.append(Kromosom(biner_anak2))    # ke populasi
 
 
-def seleksi_survivor():     #fungsi seleksi survivo agar populasi terus sama
-    populasi.sort(key=lambda c: h(c.x1, c.x2), reverse=True)
+def seleksi_survivor():     #fungsi seleksi survivor agar populasi terus sama
+    populasi.sort(key=lambda c: heuristic(c.x, c.y), reverse=True)  # memakai fungsi key dan fungsi lambda sebagai anonymous function di dalam fungsi sort
 
-    while len(populasi) != 50:
+    while len(populasi) != 50:  # kontrol jumlah populasi agar tetap
         populasi.pop()
 
 # fungsi main
@@ -93,14 +94,14 @@ while len(populasi) != 50:
 
 seleksi_survivor()
 print('Generasi', generasi)
-print('fittest', populasi[0])
+print('Best', populasi[0])
 
 arr_fit = []
-while generasi < 48:
+while generasi < 120:
     orangtua = seleksi_orangtua(2)
     Crossover(orangtua[0], orangtua[1])
     seleksi_survivor()
 
     generasi += 1
     print('Generasi', generasi)
-    print('fittest', populasi[0])
+    print('Best', populasi[0])
